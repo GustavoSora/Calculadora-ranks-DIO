e21d3de0b7d71729cb8cc3988d0e2fc36420e746
GustavoSora <151575299+GustavoSora@users.noreply.github.com>
2024-07-10 16:22:40 -0300

    Update calculadoraRanqueada.js

--- calculadoraRanqueada.js
+++ calculadoraRanqueada.js
@@ -1,26 +1,27 @@
 function stats(win, defeat) {
-    let saldoVitorias = win - defeat;
-    return saldoVitorias;
+    let saldoVitorias = win - defeat
+    return saldoVitorias
 }
 
-let saldo = stats(100, 30);
-let rank = saldo;  
-let nivel;  
+let saldo = stats(100, 30)
+let rank = saldo
+let nivel
 
 if (rank <= 10) {
-    nivel = "ferro";
+    nivel = "ferro"
 } else if (rank >= 11 && rank <= 20) {
-    nivel = "bronze";
+    nivel = "bronze"
 } else if (rank >= 21 && rank <= 50) {
-    nivel = "prata";
+    nivel = "prata"
 } else if (rank >= 51 && rank <= 80) {
-    nivel = "ouro";
+    nivel = "ouro"
 } else if (rank >= 81 && rank <= 90) {
-    nivel = "diamante";
+    nivel = "diamante"
 } else if (rank >= 91 && rank <= 100) {
-    nivel = "lendario";
+    nivel = "lendario"
 } else if (rank >= 101) {
-    nivel = "imortal";
+    nivel = "imortal"
 }
 
-console.log(`O herói tem o saldo de ${saldo} e está no nível de ${nivel}`);
+console.log(`O herói tem o saldo de ${saldo} e está no nível de ${nivel}`)
+
